1c5fa40815dfdbcc7db3f0e02810d44ce4f3f22e
RockTitan <rocktitan.cv98g@g2.nctu.edu.tw>
2018-11-15 00:51:05 +0800

    first commit

---
+++ LunchLotteryApp/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace LunchLotteryApp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.listBoxList = new System.Windows.Forms.ListBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.buttonInput = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonExit.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(372, 382);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 30);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRun.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRun.Location = new System.Drawing.Point(266, 382);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(100, 30);
+            this.buttonRun.TabIndex = 1;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelQuestion.Location = new System.Drawing.Point(12, 20);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(201, 27);
+            this.labelQuestion.TabIndex = 2;
+            this.labelQuestion.Text = "今天想吃什麼?";
+            // 
+            // listBoxList
+            // 
+            this.listBoxList.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBoxList.FormattingEnabled = true;
+            this.listBoxList.ItemHeight = 16;
+            this.listBoxList.Location = new System.Drawing.Point(17, 118);
+            this.listBoxList.Name = "listBoxList";
+            this.listBoxList.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxList.Size = new System.Drawing.Size(349, 244);
+            this.listBoxList.TabIndex = 3;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxInput.Location = new System.Drawing.Point(84, 69);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(282, 27);
+            this.textBoxInput.TabIndex = 4;
+            this.textBoxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_KeyDown);
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelInput.Location = new System.Drawing.Point(14, 72);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(64, 16);
+            this.labelInput.TabIndex = 5;
+            this.labelInput.Text = "輸入 : ";
+            // 
+            // buttonInput
+            // 
+            this.buttonInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonInput.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInput.Location = new System.Drawing.Point(372, 68);
+            this.buttonInput.Name = "buttonInput";
+            this.buttonInput.Size = new System.Drawing.Size(100, 30);
+            this.buttonInput.TabIndex = 6;
+            this.buttonInput.Text = "加入";
+            this.buttonInput.UseVisualStyleBackColor = true;
+            this.buttonInput.Click += new System.EventHandler(this.buttonInput_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDelete.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(372, 232);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(100, 30);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "刪除";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStripMain.Location = new System.Drawing.Point(0, 426);
+            this.statusStripMain.MinimumSize = new System.Drawing.Size(0, 35);
+            this.statusStripMain.Name = "statusStripMain";
+            this.statusStripMain.Size = new System.Drawing.Size(484, 35);
+            this.statusStripMain.TabIndex = 8;
+            this.statusStripMain.Text = "statusStripMain";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(16, 17);
+            this.toolStripStatusLabel1.Text = " ";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.statusStripMain);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonInput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.listBoxList);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.buttonExit);
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "FormMain";
+            this.Text = "Lunch Lottery";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.statusStripMain.ResumeLayout(false);
+            this.statusStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.ListBox listBoxList;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.Button buttonInput;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ LunchLotteryApp/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LunchLotteryApp
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            listBoxList.Items.Add("CUCO");
+            listBoxList.Items.Add("大蘋果");
+            listBoxList.Items.Add("福來");
+            listBoxList.Items.Add("Mama");
+            listBoxList.Items.Add("吳家豆漿");
+            listBoxList.Items.Add("德東49號食堂(前好粥到)");
+            listBoxList.Items.Add("巷仔口(蛋包飯)");
+            listBoxList.Items.Add("胡家小吃");
+            listBoxList.Items.Add("忠誠牛肉麵");
+            listBoxList.Items.Add("黃家牛肉麵");
+            listBoxList.Items.Add("老北投");
+            listBoxList.Items.Add("Subber");
+        }
+
+        private void textBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddItem();
+            }
+        }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+            AddItem();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            DeleteItem();
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            List<int> intList = new List<int>();
+            for (int i = 0; i < 10000000; i++)
+            {
+                int varValue = random.Next();
+                do
+                {
+                    varValue = varValue % listBoxList.Items.Count;
+                }
+                while (varValue > listBoxList.Items.Count);
+                intList.Add(varValue);
+            }
+
+            var res = from n in intList
+                      group n by n into g
+                      orderby g.Count() descending
+                      select g;
+            var gr = res.First();
+
+            int index = 0;
+            foreach (int x in gr)
+            {
+                index = x;
+            }
+
+            MessageBox.Show(listBoxList.Items[index].ToString());
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddItem()
+        {
+            bool comp = false;
+
+            if (textBoxInput.Text.Trim() != string.Empty)
+            {
+                foreach (var item in listBoxList.Items)
+                {
+                    if (textBoxInput.Text.Trim() == item.ToString())
+                    {
+                        comp = true;
+                    }
+                }
+                if (comp == false)
+                {
+                    listBoxList.Items.Add(textBoxInput.Text.Trim());
+                }
+            }
+        }
+
+        private void DeleteItem()
+        {
+            List<string> tempList = new List<string>();
+
+            foreach (string item in listBoxList.Items)
+            {
+                tempList.Add(item.ToString());
+            }
+
+            foreach (var item in listBoxList.SelectedItems)
+            {
+                tempList.Remove(item.ToString());
+            }
+
+            listBoxList.Items.Clear();
+            foreach (var item in tempList)
+            {
+                listBoxList.Items.Add(item);
+            }
+        }
+    }
+}
